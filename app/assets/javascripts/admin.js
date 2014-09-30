0a76821711773e0b1ba908d7a022096224cbd707
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-09-30 23:27:24 +0400

    fix jss and layouts

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,10 +2,12 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require ckeditor/init
+//= require jquery.ui.nestedSortable
+//= require sortable_tree/initializer
 //= require jquery_nested_form
 //= require jquery.jcrop
 //= require papercrop
-//= require bootstrap
+//= require bootstrap-sprockets
 
 $(document).ready(function() {
     $('a').tooltip();
