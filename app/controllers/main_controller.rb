2491aa9f801f4849d4a03092985246d6b319fdd3
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-10-01 17:28:37 +0400

    fix main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,7 @@
 class MainController < ApplicationController
-
   def index
     @carousels = Carousel.all
     @main_offers = MainOffer.all
     @categories = Category.all
-    render layout: false
   end
-
 end
