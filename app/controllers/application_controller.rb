d916ec4e164ec31f53ec1c1676d8af675af13aaa
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-10-11 21:35:07 +0600

    fix footer and navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def pages
+    @pages ||= Page.order(:title)
+  end
+  helper_method :pages
 end
