23635c258ed6a28131ba8b59110d911720b757af
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2014-10-11 22:44:15 +0600

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011143115) do
+ActiveRecord::Schema.define(version: 20141011154128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -165,6 +165,7 @@ ActiveRecord::Schema.define(version: 20141011143115) do
     t.boolean  "enabled",    default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "row_order"
   end
 
   create_table "sizes", force: true do |t|
