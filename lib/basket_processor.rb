3ca61e8f16949c103c50b8cf607f7ad51f7949f3
rossmar <roman-bujenko@yandex.ru>
2014-10-14 23:23:27 +0800

    - add basket processor to process basket cookies

---
+++ lib/basket_processor.rb
@@ -0,0 +1,56 @@
+class BasketProcessor
+
+  attr_reader :goods_wrappers, :total_price
+
+  def initialize(basket)
+    @parsed_data = basket.blank? ? [] : JSON.parse(basket)
+    init_goods
+    check_goods
+    calculate_total_price
+  end
+
+  def init_goods
+    @goods_wrappers = []
+    @parsed_data.each do |good_info|
+      @goods_wrappers << BasketGoodWrapper.new(good_info['id'], good_info['size'])
+    end
+  end
+
+  def check_goods
+    @goods_wrappers.each do |wrapper|
+      wrapper.check_existence_and_init
+    end
+  end
+
+
+  def goods_count
+    @goods_wrappers.select{|w| w.exist}.count
+  end
+
+  def any_goods?
+    goods_count > 0
+  end
+
+  def calculate_total_price
+    @total_price = @goods_wrappers.select{|w| w.exist}.sum{|w| w.good.price}
+  end
+
+end
+
+class BasketGoodWrapper
+
+  attr_accessor :id, :size, :exist, :good
+
+  def initialize(id, size)
+    @id = id
+    @size = size
+    @exist = false
+  end
+
+  def check_existence_and_init
+    row = Good.where(id: @id).first
+    @exist = !row.blank?
+    @good = row.blank? ? nil : row
+  end
+
+end
